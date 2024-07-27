fae2a5821301874823225f9aef0e1204f9505673
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2024-07-27 13:35:32 +0700

    clean prints

--- looped_experiments/models.py
+++ looped_experiments/models.py
@@ -27,12 +27,10 @@ class TransformerBase(nn.Module):
 
     def _init_weights(self, module):
         if isinstance(module, nn.Linear):
-            print(f"Initializing {module}")
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
             if module.bias is not None:
                 torch.nn.init.zeros_(module.bias)
         elif isinstance(module, nn.Embedding):
-            print(f"Initializing emb {module}")
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def _init_all_params(self):
